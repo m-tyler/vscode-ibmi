658d4a60ac0f944c4ca7ec98df213f5c58f2a625
Matt <matt.tyler@wincofoods.com>
2023-09-11 16:27:01 -0600

    Update splfBrowser.ts

--- src/views/splfBrowser.ts
+++ src/views/splfBrowser.ts
@@ -613,16 +613,20 @@ export default class SPLFBrowser implements vscode.TreeDataProvider<any> {
    * @param {vscode.CancellationToken} token
    * @returns {ProviderResult<TreeItem>};
    */
-  async resolveTreeItem(item :SPLF, element :any, token :vscode.CancellationToken) :Promise<vscode.TreeItem>
+  async resolveTreeItem(item :UserSpooledFiles, element :any, token :vscode.CancellationToken) :Promise<vscode.TreeItem>
   {
     const content = getContent();
-    const splfNum = await content.getUserSpooledFileCount(item.name);
-    const userText = await content.getUserProfileText(item.name);
+    const splfNum = await content.getUserSpooledFileCount(item.user);
+    const userText = await content.getUserProfileText(item.user);
+    // const splfNum = item.user;
+    // const userText = item.user;
+    const ltoken = token;
+    const lelement = element;
     item.tooltip = ``
       .concat(userText ?  t(`splfBrowser.resolveTreeItem.toolTip1`,userText) :``)
-      .concat(userText ?  t(`splfBrowser.resolveTreeItem.toolTip2`,userText) :``)
-      .concat(`${userText ? `User Text:\t\t\t  ${userText}` : ``}`)
-      .concat(`${splfNum  ? `\nSpooled Fiile Count: ${splfNum}` : ``}`)
+      .concat(userText ?  t(`splfBrowser.resolveTreeItem.toolTip2`,splfNum) :``)
+      // .concat(`${userText ? `User Text:\t\t\t  ${userText}` : ``}`)
+      // .concat(`${splfNum  ? `\nSpooled Fiile Count: ${splfNum}` : ``}`)
     return item;
   }
 }
